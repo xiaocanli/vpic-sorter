8e7caeb33d3f76cc23a0ccbe43ed81529072e57b
Xiaocan Li <phyxiaolee@gmail.com>
2016-07-06 17:32:44 -0600

    New script to get particle tracer time series

--- multi_steps_tracer.sh
+++ multi_steps_tracer.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# export filepath=/net/scratch3/xiaocanli/tracer_test_avg/tracer_hdf5
-export filepath=/net/scratch3/guofan/turbulent-sheet3D-mixing-trinity-Jan29-test/tracer
+export filepath=/net/scratch3/xiaocanli/tracer_test_avg/tracer_hdf5
 
 # # Find the maximum time step
 # export tstep_max=-1
@@ -27,31 +26,30 @@ IFS=$'\n' tsorted=($(sort -n <<<"${tsteps[*]}"))
 # Time interval
 tinterval=`expr ${tsorted[1]} - ${tsorted[0]}`
 
-export particle=ion
-tstep_min=0
-tstep_max=2639
-tinterval=7
+export particle=electron
+tstep_min=1000
+tstep_max=2000
+tinterval=1000
 is_recreate=1 # recreate a file?
-# nsteps=$tinterval
-nsteps=1
+nsteps=$tinterval
 echo "Maximum time step:" $tstep_max
 echo "Time interval:" $tinterval
 
-key_index=17 # Sort by energy
+key_index=13 # Sort by energy
 tstep=$tstep_max
 tstep_max1=`expr $tstep_max - 1`
-mpirun -np 16 ./h5group-sorter -f $filepath/T.$tstep/${particle}_tracer_reduced_sorted.h5p \
+mpirun -np 16 ./h5group-sorter -f $filepath/T.$tstep/${particle}_tracer.h5p \
 -o $filepath/T.$tstep/${particle}_tracer_energy_sorted.h5p \
--g /Step#$tstep -m $filepath/T.$tstep/grid_metadata_${particle}_tracer_reduced.h5p \
+-g /Step#$tstep_max1 -m $filepath/T.$tstep/grid_metadata_${particle}_tracer.h5p \
 -k $key_index -a attribute --tmin=$tstep_min --tmax=$tstep_max \
 --tinterval=$tinterval --filepath=$filepath --species=${particle} -u 6 \
 --is_recreate=$is_recreate --nsteps=$nsteps
 
-key_index=16 # sort by particle tag
-mpirun -np 16 ./h5group-sorter -f $filepath/T.$tstep/${particle}_tracer_reduced_sorted.h5p \
+key_index=12 # sort by particle tag
+mpirun -np 16 ./h5group-sorter -f $filepath/T.$tstep/${particle}_tracer.h5p \
 -o $filepath/T.$tstep/${particle}_tracer_energy_sorted.h5p \
--g /Step#$tstep -m $filepath/T.$tstep/grid_metadata_${particle}_tracer_reduced.h5p \
+-g /Step#$tstep -m $filepath/T.$tstep/grid_metadata_${particle}_tracer.h5p \
 -k $key_index -a attribute --tmin=$tstep_min --tmax=$tstep_max \
 --tinterval=$tinterval --filepath=$filepath --species=${particle} \
--p -q -w -r -u 6 --filename_traj=data/${particle}s_2.h5p \
+-p -q -w -u 6 --filename_traj=data/${particle}s_2.h5p \
 --nptl_traj=1000 --ratio_emax=1 --is_recreate=$is_recreate --nsteps=$nsteps
