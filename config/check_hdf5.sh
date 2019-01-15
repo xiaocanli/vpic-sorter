cffcaa06b82ccd948359a9623c06afca094801cf
Xiaocan Li <phyxiaolee@gmail.com>
2019-01-15 15:20:27 -0700

    Update script to check a HDF5 tracer file

--- config/check_hdf5.sh
+++ config/check_hdf5.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 # Modify as you need
-export filepath=/net/scratch3/xiaocanli/reconnection/open3d-full/tracer_test/
+# export filepath=/net/scratch3/xiaocanli/reconnection/open3d-full/tracer_test/
+# export filepath=/lustre/scratch4/turquoise/xiaocanli/fan_prl/tracking_particle/tracer/tracer1
+export filepath=/lustre/scratch3/turquoise/xiaocanli/reconnection/Cori_runs/test_new_tracking/tracer/tracer1
 export particle=electron
-tstep=13
+tstep=0
 
 # May not need to change
 mpi_size=1
 fpath=$filepath/T.$tstep
-input_file=$fpath/${particle}_tracer_reduced_sorted.h5p
+# input_file=$fpath/${particle}_tracer_reduced_sorted.h5p
+# input_file=$fpath/${particle}_tracer.h5p
+input_file=$fpath/tracers.h5p
+group_name=/Step#$tstep/${particle}_tracer
+subgroup_name=${particle}_tracer
 
 cd ../
 
-srun -n $mpi_size ./check_hdf5 $input_file /Step#$tstep
+srun -n $mpi_size ./check_hdf5 $input_file $group_name $subgroup_name
 
 cd config
